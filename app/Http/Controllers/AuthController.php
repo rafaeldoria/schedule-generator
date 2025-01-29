998b1ff97734b5b8935282594769ea178314a5eb
rafaeldoria <ieieldoria@gmail.com>
2025-01-29 16:09:27 -0300

    #02 - Create authorization routes.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) : JsonResponse
+    {
+        $registerUserData = $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|min:8'
+        ]);
+
+        User::create([
+            'name' => $registerUserData['name'],
+            'email' => $registerUserData['email'],
+            'password' => Hash::make($registerUserData['password']),
+        ]);
+
+        return response()->json([
+            'message' => 'User Created ',
+        ]);
+    }
+    public function login(Request $request): JsonResponse
+    {
+        $user = User::where('email',  $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password))
+        {
+            return response()->json([
+                'message' => ['Username or password incorrect'],
+            ]);
+        }
+
+        $user->tokens()->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User logged in successfully',
+            'name' => $user->name,
+            'token' => $user->createToken('auth_token')->plainTextToken,
+        ]);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User logged out successfully'
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @method static where(string $string, mixed $email)
+ * @method static create(array $array)
+ */
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    /** @use HasFactory<UserFactory> */
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
